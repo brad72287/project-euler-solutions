453a9171772c2ad965fa0ec2e4f028cbe5fc017d
brad <bradleyreiss@iAndrew.local>
2015-11-27 12:11:36 +0530

    in progress...
    need a faster way to detect primes :(

---
+++ 3.rb
@@ -0,0 +1,18 @@
+def prime?(x)
+  (2..(x/2)).each do |number|
+    return false if x%number==0
+  end
+  return true 
+end
+
+def prime_factors(x)
+  factors=[]
+  (1..(x/2)).each do |number|
+    factors << number if x%number==0 && prime?(number)
+  end
+  factors
+end
+
+p (1..100).select {|x| prime?(x)}
+
+p prime_factors(600851)
